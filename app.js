609216da86ac2ff246e1902a6441bd39379196c9
Mohamed Maaiz <mohammadh3g@gmail.com>
2021-09-06 13:59:06 +0500

    added a smiple ui nd some changes to code

--- app.js
+++ app.js
@@ -1,67 +1,78 @@
-let wepons = ["rock","paper","scissor"];
+//play buttons
+const rbt = document.querySelector('#rockBT');
+rbt.addEventListener('click', () => {
+    playRound(playerSelection='rock',computerPlay());
+});
+
+const pbt = document.querySelector('#paperBT');
+pbt.addEventListener('click', () => {
+    playRound(playerSelection='paper',computerPlay());
+});
+
+const sbt = document.querySelector('#scissorBT');
+sbt.addEventListener('click', () => {
+    playRound(playerSelection='scissor',computerPlay());
+});
 
 //computer making its choice
 function computerPlay(){
+    let wepons = ["rock","paper","scissor"];
     let computerChoice = wepons[Math.floor(Math.random() * wepons.length)];
     return computerChoice;
 }
+
+//variables
+let pScore = 0;
+let cScore = 0;
+let message = '';
    
 //cheking results
 const playRound = (playerSelection, computerSelection) => {
 
     if (playerSelection === "rock" && computerSelection === "paper") {
-        return `
-        You: ${playerSelection}
-        Computer: ${computerSelection+"\n"}
-        You Lose! Paper beats Rock`
+        cScore+=1;
+        message = 'You Lose! Paper beats Rock';     
     } else if (playerSelection === "rock" && computerSelection === "scissor"){
-        return `
-        You: ${playerSelection}
-        Computer: ${computerSelection+"\n"}
-        You Won!! Rock beats Scissors`
+        pScore+=1;
+        message = 'You Won!! Rock beats Scissors';
     } else if (playerSelection === "paper" && computerSelection === "rock"){
-        return `
-        You: ${playerSelection}
-        Computer: ${computerSelection+"\n"}
-        You Won!! Paper beats Rock`
+        pScore+=1;
+        message = 'You Won!! Paper beats Rock';
     } else if (playerSelection === "paper" && computerSelection === "scissor"){
-        return `
-        You: ${playerSelection}
-        Computer: ${computerSelection+"\n"}
-        You Lose! Scissors beats Paper`
+        cScore+=1;
+        message = 'You Lose! Scissors beats Paper';
     } else if (playerSelection === "scissor" && computerSelection === "rock"){
-        return `
-        You: ${playerSelection}
-        Computer: ${computerSelection+"\n"}
-        You Lose! Rock beats Scissors`
+        cScore+=1;
+        message = 'You Lose! Rock beats Scissors';
     } else if (playerSelection === "scissor" && computerSelection === "paper"){
-        return `
-        You: ${playerSelection}
-        Computer: ${computerSelection+"\n"}
-        You Won!! Scissors beats Paper`
+        pScore+=1;
+        message = 'You Won!! Scissors beats Paper';
     } else {
-        return `
-        You: ${playerSelection}
-        Computer: ${computerSelection+"\n"}
-        It's a Draw, Try again`
+        message = "It's a Draw, Try again";
+    }
+    
+    displayBoard (playerSelection,computerSelection)
+}
+
+//display board
+function displayBoard (playerSelection,computerSelection){
+    //display choices
+    const resultDisplay = document.querySelector('.choices');
+    resultDisplay.innerHTML = `<p>You:  ${playerSelection}<br>Comp: ${computerSelection}<br><br>${message}</p>`
+
+    //display score
+    const scoreBoard = document.querySelector('.result');
+    scoreBoard.innerHTML = `<p>${pScore}<br>${cScore}</p>`;
+
+    if (pScore > 4){
+        pScore = 0;
+        cScore = 0;
+        scoreBoard.innerHTML = `<h2>You Won This Round</h2>`
+    } else if (cScore > 4){
+        pScore = 0;
+        cScore = 0;
+        scoreBoard.innerHTML = `<h2>Comp Won This Round</h2>`
     }
 }
 
-//play function
-function game() {
-    for (i = 0; i < 5; i++) {
-        let playerSelection = prompt("select a choice\nrock - paper - scissor")
-        playerSelection = playerSelection.toLowerCase();
-        
-        //cheking if the choice was acceptable 
-        if (wepons.includes(playerSelection)) {
-            const computerSelection = computerPlay();
-            alert(playRound(playerSelection, computerSelection));
-        } else {
-            alert("wrong choice!!!")
-        }    
-    }
-}
 
-//launch the game
-game();
